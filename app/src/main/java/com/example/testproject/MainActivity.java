c3a590cc62923f13ec75253162d56faee72091b5
fathimar <fathimarahiyanath2000@gmail.com>
2021-01-09 14:24:03 +0530

    test project

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.testproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.uname);
+        password=(EditText)findViewById(R.id.pass);
     }
+
+    public void myclickevent(View view) {
+        String getusername=username.getText().toString();
+        String getpassword=password.getText().toString();
+        Toast.makeText(this,getusername+getpassword,Toast.LENGTH_LONG).show();
+    }
+
 }
